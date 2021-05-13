0637ebeed11192612d6dc223baac6461d3cda8c5
MadelineMartin <martin.mjm105@gmail.com>
2021-05-12 21:12:13 -0400

    started workshop 1

---
+++ ourcodingclub_tutorials/tutorial_1.R
@@ -0,0 +1,7 @@
+# Coding Club Workshop 1 - R Basics
+# Learning how to import and explore data, and make graphs about Edinburgh's biodiversity
+# Written by Gergana Daskalova 06/11/2016 University of Edinburgh
+install.packages("dplyr")
+library(dplyr)
+# Note that there are quotation marks when installing a package, but not when loading it
+# and remember that hashtags let you add useful notes to your code! 
