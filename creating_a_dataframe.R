4906912d00a6a1230f79d75299cd79273961d899
MadelineMartin <martin.mjm105@gmail.com>
2021-06-24 17:28:26 -0400

    dataframe exercise

---
+++ creating_a_dataframe.R
@@ -0,0 +1,4 @@
+# Perhaps as an exercise, see if you can make a data frame that has two columns the first with “treatment” with values 
+# 1 through 5 and the second with “response” with random gaussian values with a different mean for each treatment. 
+# So there are say 100 rows for each treatment, so the data frame frame has 500 rows total.
+
